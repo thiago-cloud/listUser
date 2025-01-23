9f327d1034a32857274aee113f0503ff5a35fbbd
thiago-cloud <thiago.154ti@gmail.com>
2025-01-23 15:29:32 -0300

    Armazenamento do código sql

--- sql/scripts.sql
+++ sql/scripts.sql
@@ -9,3 +9,25 @@ create TABLE usuario(
     password character varying(50),
     CONSTRAINT usuario_pkey PRIMARY KEY (id)
 )
+
+
+CREATE TABLE papel
+(
+    id bigserial NOT NULL,
+    tipo_papel character varying(50),
+    CONSTRAINT papel_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE usuario_papel
+(
+    usuario_id bigint NOT NULL,
+    papel_id bigint NOT NULL,	  
+    CONSTRAINT fk_usuario_papel_papel_id FOREIGN KEY (papel_id)
+        REFERENCES papel (id) ON DELETE CASCADE,
+    CONSTRAINT fk_usuario_papel_usuario_id FOREIGN KEY (usuario_id)
+        REFERENCES usuario (id) ON DELETE CASCADE 
+);
+
+INSERT INTO papel (tipo_papel) VALUES ('ADMIN');
+INSERT INTO papel (tipo_papel) VALUES ('USER');
+INSERT INTO papel (tipo_papel) VALUES ('BIBLIO');
