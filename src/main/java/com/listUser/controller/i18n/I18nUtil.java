703a1050597bf787a77b433211d7b92253e98910
thiago-cloud <thiago.154ti@gmail.com>
2025-01-23 15:30:15 -0300

    Inclusão de um comentário

--- src/main/java/com/listUser/controller/i18n/I18nUtil.java
+++ src/main/java/com/listUser/controller/i18n/I18nUtil.java
@@ -7,7 +7,7 @@ public class I18nUtil {
 	
 	public String getMensagem(Locale locale, String chave) {		
 		
-		// O resources.message faz referencia a pasta e aos arquivos de tradução e o e variavél que pode ser pt-BR ou en-US
+		// O resources.message faz referencia a pasta e aos arquivos de tradução e a variavél que pode ser pt-BR ou en-US
 	    ResourceBundle bundle = ResourceBundle.getBundle("resources.message", locale);
 
 	    // A variavél mensagem vai pegar o arquivo de tradução de acordo com lógicae inserir o parametro chave que no caso e mensagem
